c54381dd2e9d6d2325b941354d43294d4bfabd91
Yoav Kleinberger <haarcuba@gmail.com>
2026-04-25 22:24:27 +0300

    initial name callback

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
-export function Counter() {
+interface Props {
+    onNameChange?: (id: string, name: string) => void;
+}
+
+export function Counter({ onNameChange }: Props) {
+    const id = React.useRef(crypto.randomUUID());
     const [name, setName] = React.useState('untitled');
     const [editing, setEditing] = React.useState(false);
+
+    React.useEffect(() => {
+        onNameChange?.(id.current, name);
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
         <div>
--- tests/Counter.test.tsx
+++ tests/Counter.test.tsx
@@ -1,9 +1,15 @@
+import { vi } from 'vitest';
 import * as reactTesting from '@testing-library/react';
 import * as Counter from '../src/Counter';
 
+const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+
 describe('Counter', () => {
-    it('renders without crashing', () => {
-        reactTesting.render(<Counter.Counter />);
+    it('initial state', () => {
+        const onNameChange = vi.fn<(id: string, name: string) => void>();
+        reactTesting.render(<Counter.Counter onNameChange={onNameChange} />);
+
+        expect(onNameChange).toHaveBeenCalledWith(expect.stringMatching(UUID_REGEX), 'untitled');
     });
 
     it('add-name', () => {
