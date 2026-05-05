40f165c7aaacf3ea6054de96e9066248807be537
Yoav Kleinberger <haarcuba@gmail.com>
2026-05-06 00:46:09 +0300

    refactor

---
+++ src/EditableName.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface Props {
+    name: string;
+    onChange: (name: string) => void;
+}
+
+export function EditableName({ name, onChange }: Props) {
+    const [inputValue, setInputValue] = React.useState(name);
+    const [editing, setEditing] = React.useState(false);
+
+    function commit() {
+        onChange(inputValue);
+        setEditing(false);
+    }
+
+    return (
+        <>
+            {!editing && <div onClick={() => setEditing(true)}>{name}</div>}
+            <input
+                style={{ display: editing ? undefined : 'none' }}
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                onBlur={commit}
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') commit();
+                }}
+            />
+        </>
+    );
+}
